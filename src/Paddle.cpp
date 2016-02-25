78fcf39085b93a164beb6320e2e3afec3e627530
Itokatsu <itokatsu@tutanota.com>
2016-02-25 20:06:14 +0100

    Inherited IMovable constructors

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -14,9 +14,6 @@ Paddle::Paddle(vec2f pos) : IMovable(pos)
 Paddle::Paddle(int x, int y) : IMovable(x, y)
 {
 	size = {100, 20};
-	// position = {float(x), float(y)};
-	// velocity = {0.f, 0.f};
-	// acceleration = {0.f, 0.f};
 }
 
 void Paddle::PrintCoords()
